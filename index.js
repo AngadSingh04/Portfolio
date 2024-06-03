11a737758c01e8cadfe5594a39f8af5a8553cdc3
Angad Singh <singh04angad@gmail.com>
2024-06-03 23:44:49 +0530

    work section updated

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function scrollFunction() {
     document.getElementById("angad-heading").style.opacity = opacity;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+    };
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("section-in-view");
+                observer.unobserve(entry.target); // Stop observing once the animation is triggered
+            }
+        });
+    }, options);
+
+    document.querySelectorAll("section").forEach(section => {
+        observer.observe(section);
+    });
+});
