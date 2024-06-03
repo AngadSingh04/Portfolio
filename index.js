17866567e9311cd85047c14f693912aacd66a70d
Angad Singh <singh04angad@gmail.com>
2024-06-03 12:00:55 +0530

    scroll js

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var scrollPosition = window.scrollY;
+    var opacity = 1 - scrollPosition / 100; // Adjust the denominator for speed of fade
+
+    document.getElementById("angad-heading").style.opacity = opacity;
+}
