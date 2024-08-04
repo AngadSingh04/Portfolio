36f817dc7d112b9b3958d58f89c41ef13c151ea9
AngadSingh04 <singh04angad@gmail.com>
2024-08-04 22:33:58 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     var scrollPosition = window.scrollY;
-    var opacity = 1 - scrollPosition / 100; // Adjust the denominator for speed of fade
+    var opacity = 1 - scrollPosition / 100; 
 
     document.getElementById("angad-heading").style.opacity = opacity;
 }
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("section-in-view");
-                observer.unobserve(entry.target); // Stop observing once the animation is triggered
+                observer.unobserve(entry.target); /
             }
         });
     }, options);
@@ -29,22 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all the links in the navigation
+   
     const navLinks = document.querySelectorAll('nav ul li a');
 
-    // Add event listeners to each link
+   
     navLinks.forEach(link => {
         link.addEventListener('click', smoothScroll);
     });
 
-    // Function to handle smooth scrolling
+   
     function smoothScroll(e) {
         e.preventDefault();
-        const targetId = this.getAttribute('href'); // Get the target section's id
-        const targetSection = document.querySelector(targetId); // Find the target section
-        const offsetTop = targetSection.offsetTop; // Get the top position of the target section
-
-        // Scroll smoothly to the target section
+        const targetId = this.getAttribute('href'); 
+        const targetSection = document.querySelector(targetId); 
+        const offsetTop = targetSection.offsetTop; 
         window.scrollTo({
             top: offsetTop,
             behavior: 'smooth'
